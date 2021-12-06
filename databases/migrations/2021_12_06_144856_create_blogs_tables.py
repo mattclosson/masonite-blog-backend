3cc1abfadcad350c45201eddbc54b84502b656a8
Matt Closson <mattwclosson@gmail.com>
2021-12-06 15:07:05 -0500

    masonite update

---
+++ databases/migrations/2021_12_06_144856_create_blogs_tables.py
@@ -0,0 +1,22 @@
+"""CreateBlogsTables Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class CreateBlogsTables(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("blogs") as table:
+            table.increments("id")
+
+            table.timestamps()
+            table.string("title")
+            table.string("body")
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("blogs")
--- routes/web.py
+++ routes/web.py
@@ -1,7 +1,17 @@
 """Web Routes."""
 
-from masonite.routes import Get, Post
+from masonite.routes import Get, Post, Put, Delete, RouteGroup
 
 ROUTES = [
     Get("/", "WelcomeController@show").name("welcome"),
+
+    ## Blogs Route Group ("/blog")
+    RouteGroup([
+        Get("/", "BlogController@index").name("index"),
+        Get("/@id", "BlogController@show").name("show"),
+        Post("/", "BlogController@create").name("create"),
+        Put("/@id", "BlogController@update").name("update"),
+        Delete("/@id", "BlogController@destroy").name("destroy")
+
+    ], prefix="/blog", name="blog")
 ]
